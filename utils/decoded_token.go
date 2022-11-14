0ea1b68ed2b2691cf6c808eab7988671feb607c4
David Solis <davidsolisdev@gmail.com>
2022-11-14 01:39:53 -0600

    v1

--- utils/decoded_token.go
+++ utils/decoded_token.go
@@ -2,16 +2,30 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func DecodedToken(token string) (claims *ClaimsJwt, err error) {
+	claims = new(ClaimsJwt)
 	t, err := jwt.Parse(token, func(tk *jwt.Token) (interface{}, error) { return []byte(os.Getenv("SECRET_SIGNED_TOKEN")), nil })
-	claims, ok := t.Claims.(*ClaimsJwt)
+
+	data, ok := t.Claims.(jwt.MapClaims)
 	if !ok {
 		return nil, errors.New("no se puede extraer la información del token")
 	}
+
+	idfloat := data["id"].(float64)
+	id, e := strconv.Atoi(fmt.Sprintf("%v", idfloat))
+	if e != nil {
+		return nil, e
+	}
+
+	(*claims).Id = uint(id)
+	//TODO: extraer datos faltantes
+
 	return claims, err
 }
